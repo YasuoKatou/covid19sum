ade09ed9dc7ac4f57d46ef47fa388dca08449319
Yasuo Katou <yasuokatou@gmail.com>
2021-07-18 15:51:56 +0900

    update db router

--- covid19sum/db_router.py
+++ covid19sum/db_router.py
@@ -4,28 +4,27 @@ class DBRouter:
 
     MyDbId = 'covid19sum'
 
-    def db_for_read(self, model, **hints):
-        print('[db_for_read] app_label : %s' % model._meta.app_label)
-        if model._meta.app_label == self.MyApp:
+    def _getMyDbId(self, name):
+        if name == self.MyApp:
             return self.MyDbId
         return None
+
+    def db_for_read(self, model, **hints):
+        print('[%s.db_for_read] app_label : %s' % (self.MyApp, model._meta.app_label, ))
+        return self._getMyDbId(model._meta.app_label)
 
     def db_for_write(self, model, **hints):
-        print('[db_for_write] app_label : %s' % model._meta.app_label)
-        if model._meta.app_label == self.MyApp:
-            return self.MyDbId
-        return None
+        print('[%s.db_for_write] app_label : %s' % (self.MyApp, model._meta.app_label, ))
+        return self._getMyDbId(model._meta.app_label)
 
     def allow_relation(self, obj1, obj2, **hints):
-        print('[allow_relation] obj1.app_label : %s' % obj1._meta.app_label)
-        print('[allow_relation] obj2.app_label : %s' % obj2._meta.app_label)
-        if (obj1._meta.app_label == MyApp or
-            obj2._meta.app_label == MyApp):
-            return True
-        return None
+        print('[%s.allow_relation] obj1.app_label : %s' % (self.MyApp, obj1._meta.app_label, ))
+        print('[%s.allow_relation] obj2.app_label : %s' % (self.MyApp, obj2._meta.app_label, ))
+        r = self._getMyDbId(obj1._meta.app_label)
+        if r:
+            return r
+        return self._getMyDbId(obj2._meta.app_label)
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        print('[allow_migrate] app_label : %s' % app_label)
-        if app_label == self.MyApp:
-            return self.MyDbId
-        return None
+        print('[%s.allow_migrate] app_label : %s' % (self.MyApp, app_label, ))
+        return self._getMyDbId(app_label)
